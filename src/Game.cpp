c4e71b14ffc8f68d6ad8b007e660a2dfa4d49103
lrewega <lrewega@ubuntu.(none)>
2012-03-30 22:25:06 -0700

    Hello world with window with cmake and magic

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char * argv [] ) {
-  std::cout << "Hello, world!" << std::endl;
-  return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Molyjam!");
+
+    sf::Text text("Hello World!");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        // draw our shit
+        window.draw(text);
+
+        window.display();
+    }
+
+    return 0;
 }
