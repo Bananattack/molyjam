4752f6d8700bb94f8b4a31aa8f2a713f37fc658e
lrewega <lrewega@ubuntu.(none)>
2012-03-30 23:38:39 -0700

    Text now flies around insanely

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+
+#include <cstdlib>
+#include <cmath>
+
+float randf() {
+    return ( (double) rand() / double(RAND_MAX) );
+}
 
 int main(int argc, char * argv [] ) {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Molyjam!");
+    sf::FloatRect screen( sf::Vector2f(0, 0), sf::Vector2f( window.getSize() ) );
 
-    sf::Text text("Hello World!");
+    std::string dict[] = {"hello", "world", "foo", "bar", "magic", "molyjam!", "stuff"};
+
+    sf::Text words[7];
+    std::vector<sf::Vector2f> vels;
+
+    for( size_t i = 0; i < 7; ++i ) {
+        words[i] = sf::Text(dict[i]);
+        words[i].setPosition( sf::Vector2f( window.getSize() ) / 2.0f );
+        float r = randf() * 2 * M_PI;
+        vels.push_back(sf::Vector2f( sin(r), -cos(r) ) );
+    }
 
     while (window.isOpen()) {
         sf::Event event;
@@ -15,7 +34,15 @@ int main(int argc, char * argv [] ) {
 
         window.clear();
         // draw our shit
-        window.draw(text);
+        for( size_t i = 0; i < 7; ++i ) {
+            words[i].move(vels[i]);
+            if( !screen.intersects( words[i].getGlobalBounds() ) ) {
+                words[i].setPosition( sf::Vector2f( window.getSize() ) / 2.0f );
+                float r = randf() * 2 * M_PI;
+                vels[i] = sf::Vector2f( sin(r), -cos(r) );
+            }
+            window.draw(words[i]);
+        }
 
         window.display();
     }
