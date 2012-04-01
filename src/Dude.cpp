0bd7eb4722ba30c86c563044dacdf2824ce1d8e2
lrewega <lrewega@ubuntu.(none)>
2012-03-31 22:09:10 -0700

    Adding fonts and better animation and magic

--- src/Dude.cpp
+++ src/Dude.cpp
@@ -8,14 +8,14 @@ Dude::Dude() :
         die_horribly();
     }
     sprite.setTexture(spritesheet);
-    sprite.setTextureRect(sf::IntRect(0,0,64,64));
     sprite.setPosition(400,300);
+    sprite.setScale(2.0f, 2.0f);
 }
 
 Dude::~Dude() {
 }
 
-enum direction {
+enum Direction {
     NONE,
     RIGHT,
     LEFT,
@@ -34,32 +34,67 @@ void Dude::step( World& world ) {
 
     sf::Vector2f pos = sprite.getPosition();
 
+    Direction dir = NONE;
     //movement!
-    static const float MOVE_ACCEL = 2.0f;
-    static const float MAX_MOVE_SPEED = 8.0f;
-    static const float MOVE_FRICTION = 0.2f;
+    static const float MOVE_ACCEL = 1.0f;
+    static const float MAX_MOVE_SPEED = 16.0f;
+    static const float MOVE_FRICTION = 0.9f;
     static const float JUMP_ACCEL = 16.0f;
-    static const float GRAVITY = 0.981f;
+    static const float GRAVITY = 2.0f;
     static const float MAX_FALL = 16.0f;
 
     if( left && !right ) {
         velocity.x -= MOVE_ACCEL;
+        dir = LEFT;
     } else if( right && !left ) {
         velocity.x += MOVE_ACCEL;
+        dir = RIGHT;
+    } else {
+        velocity.x *= MOVE_FRICTION;
+        dir = NONE;
     }
-/*
+
+    velocity.x = std::min( std::max( velocity.x, -MAX_MOVE_SPEED ), MAX_MOVE_SPEED );
+    if( velocity.x > 6.5f ) {
+        dir = RIGHT;
+    } else if( velocity.x < -6.5f ) {
+        dir = LEFT;
+    }
     if( jump ) {
-        if( !is_jumping && can_jump ) {
-            //jump
-        } else if
-*/
+        if( can_jump ) {
+            can_jump = false;
+//            is_jumping = true;
+            velocity.y = -JUMP_ACCEL;
+        } else {
+            velocity.y += GRAVITY * 0.3f;
+        }
+    } else {
+        velocity.y += GRAVITY;
+    }
 
+    velocity.y = std::min( velocity.y, MAX_FALL );
     pos += velocity;
-
     // bounding!
+    sprite.setPosition( pos );
+    sf::FloatRect bounds = sprite.getGlobalBounds();
+    if( bounds.top + bounds.height  > 600 ) {
+        pos.y = 600 - bounds.height;
+        can_jump = true;
+    }
 
-    // done!
-    sprite.setPosition(pos);
+    sprite.setPosition( pos );
+    switch(dir) {
+        default:
+        case NONE:
+            sprite.setTextureRect(sf::IntRect(0,0,48,32));
+            break;
+        case LEFT:
+            sprite.setTextureRect(sf::IntRect(0,32,48,32));
+            break;
+        case RIGHT:
+            sprite.setTextureRect(sf::IntRect(0,64,48,32));
+            break;
+    }
 }
 
 void Dude::render( sf::RenderWindow& window ) {
